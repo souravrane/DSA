c152071b35ccf708820971dda0d21868b21ea59d
souravrane <sourav1121997@gmail.com>
2021-10-11 17:47:58 +0530

    javascript solution for two odd occuring digits

---
+++ Bit Magic/5. Two odd occuring digits/code.js
@@ -0,0 +1,23 @@
+const twoOddOccuringDigits = function (arr, n) {
+  let XOR = 0,
+    digit1 = 0,
+    digit2 = 0;
+
+  for (let i = 0; i < n; i++) {
+    XOR = XOR ^ arr[i];
+  }
+
+  let lastSetBit = XOR & ~(XOR - 1); // most important step of the algorithm
+
+  for (i = 0; i < n; i++) {
+    if ((arr[i] & lastSetBit) != 0) {
+      digit1 = digit1 ^ arr[i];
+    } else {
+      digit2 = digit2 ^ arr[i];
+    }
+  }
+  return [digit1, digit2];
+};
+
+arr = [1, 1, 1, 1, 2, 2, 3, 4, 5, 5];
+console.log(twoOddOccuringDigits(arr, arr.length));
