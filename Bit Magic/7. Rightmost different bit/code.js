b18437a831d025c36f057f03fedaec011bf0730c
souravrane <sourav1121997@gmail.com>
2021-10-12 19:10:33 +0530

    right most different bit

---
+++ Bit Magic/7. Rightmost different bit/code.js
@@ -0,0 +1,16 @@
+const rightmostDiffBit = function (n, m) {
+  if (m === n) return -1;
+  let position = 0;
+  while (m || n) {
+    if (m % 2 === n % 2) {
+      position++;
+      m = m >> 1;
+      n = n >> 1;
+    } else return position;
+  }
+};
+
+const num1 = 5,
+  num2 = 11;
+
+console.log(rightmostDiffBit(num1, num2));
