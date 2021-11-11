b0631fca3fdbdc31e1a2c86cfd65cdc482d29720
souravrane <sourav1121997@gmail.com>
2021-11-11 23:37:36 +0530

    binary search in progress...

---
+++ Searching/1. Binary Search/code.js
@@ -0,0 +1,8 @@
+const binarySearch = function (arr, n) {
+  let low = 0,
+    high = n - 1,
+    mid = Math.floor((low + high) / 2);
+};
+
+const arr = [10, 20, 30, 40, 50, 60, 70];
+console.log(binarySearch(arr, arr.length));
