b51551f7ebd449d56192ec57350324628693f166
souravrane <sourav1121997@gmail.com>
2021-11-14 13:00:30 +0530

    binary search

--- Searching/1. Binary Search/code.js
+++ Searching/1. Binary Search/code.js
@@ -2,6 +2,19 @@ const binarySearch = function (arr, n) {
   let low = 0,
     high = n - 1,
     mid = Math.floor((low + high) / 2);
+
+  while (low <= high) {
+    mid = Math.floor((low + high) / 2);
+
+    if (arr[mid] < x) {
+      low = mid + 1;
+    } else if (arr[mid] > x) {
+      high = mid - 1;
+    } else {
+      return mid;
+    }
+  }
+  return -1;
 };
 
 const arr = [10, 20, 30, 40, 50, 60, 70];
